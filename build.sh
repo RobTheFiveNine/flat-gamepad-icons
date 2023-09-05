8a78f55a85d96643e71abddcef287ccd7af8990c
RobTheFiveNine <robthefivenine@proton.me>
2023-09-05 21:38:05 +0100

    Update build script to output into dpi specific folders

--- build.sh
+++ build.sh
@@ -3,20 +3,24 @@ declare -a dpis=("50" "96" "150" "300")
 ids=$(grep -oP 'id="(generic|xb|ps|switch|kb)_.+?"' icons.svg | cut -d '"' -f 2)
 
 mkdir -p build
-rm build/*
+rm -rf build/*
 
 for dpi in "${dpis[@]}"
 do
+    mkdir -p "build/${dpi}dpi"
     while read id; do
         echo "Building $id @ $dpi dpi..."
-        inkscape --export-id="$id" --export-id-only -d "$dpi" --export-type=png -o "build/${id}.${dpi}dpi.png" icons.svg 2> /dev/null
+        inkscape --export-id="$id" --export-id-only -d "$dpi" --export-type=png -o "build/${dpi}dpi/${id}.png" icons.svg 2> /dev/null
     done <<< "$ids"
 done
 
 cp LICENSE.dist.txt build/LICENSE.txt
 
 echo "Compressing to flat-gamepad-icons.zip..."
-zip -q -j -9 flat-gamepad-icons.zip build/*
+rm -f flat-gamepad-icons.zip
+cd build
+zip -q -r -9 ../flat-gamepad-icons.zip *
+cd ..
 
 
 echo "Done."
